14c726fca68fa033e9ae96d6b614e85fe6b5a768
Subin <39542978+subeans@users.noreply.github.com>
2022-05-24 13:27:01 +0900

    add metrics and delete unused library

--- GPU/inference.py
+++ GPU/inference.py
@@ -1,9 +1,7 @@
-import os, re, time, json
-import PIL.Image, PIL.ImageFont, PIL.ImageDraw
+import os, time
 import numpy as np
 import tensorflow as tf
 import pandas as pd
-from matplotlib import pyplot as plt
 from tensorflow.keras.models import load_model
 from tensorflow.keras.applications import (
     xception,
@@ -45,22 +43,6 @@ models = {
 #     'efficientnet_b7':efficientnet
 }
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-m','--model_type', required=True)
-print(parser)
-parser.add_argument('-l', '--batch_list',
-                      nargs='+',
-                      required=True)
-
-model_type = parser.parse_args().model_type
-batch_list = parser.parse_args().batch_list
-os.environ["CUDA_VISIBLE_DEVICES"]="0"
-gpus = tf.config.experimental.list_physical_devices('GPU')
-if gpus:
-    try:
-        tf.config.experimental.set_memory_growth(gpus[0], True)
-    except RuntimeError as e:
-        print(e)
 
 def deserialize_image_record(record):
     feature_map = {'image/encoded': tf.io.FixedLenFeature([], tf.string, ''),
@@ -91,18 +73,18 @@ def val_preprocessing(record):
     new_width = tf.cast(tf.math.rint(width * scale), tf.int32)
     
     image = tf.image.resize(image, [new_height, new_width], method='bicubic')
-    if "inception" in model_type or "xception" in model_type:
+    if "inception" in model or "xception" in model:
         image = tf.image.resize_with_crop_or_pad(image, 299, 299)
     else:
         image = tf.image.resize_with_crop_or_pad(image, 224, 224)
 
     label = tf.cast(label, tf.int32)
-    image = models[model_type].preprocess_input(image)
+    image = models[model].preprocess_input(image)
     image = tf.cast(image, tf.float32)
     return image, label
 
 def get_dataset(batch_size, use_cache=False):
-    data_dir = '../datasets/images-1000/*'
+    data_dir = '/home/ubuntu/datasets/*'
     files = tf.io.gfile.glob(os.path.join(data_dir))
     dataset = tf.data.TFRecordDataset(files)
     
@@ -125,10 +107,12 @@ def inference(saved_model_name, batch_size):
     warm_up = 10
     
     ds = get_dataset(batch_size)
+
     load_start = time.time()
     model = load_model(saved_model_name)
     load_time = time.time() - load_start
     counter = 0
+
     with tf.device("/device:GPU:0"):
         for batch, batch_labels in ds:
             if counter == 0:
@@ -137,6 +121,7 @@ def inference(saved_model_name, batch_size):
 
             start_time = time.time()
             yhat_np = model.predict(batch)
+
             if counter ==0:
                 first_iter_time = time.time() - start_time
             else:
@@ -152,32 +137,51 @@ def inference(saved_model_name, batch_size):
             
             if counter == 100:
                 break
+
         iter_times = np.array(iter_times)
-        acc_tpu = np.sum(np.array(actual_labels) == np.array(pred_labels))/len(actual_labels)
-        results = pd.DataFrame(columns = [f'GPU_{model_type}_{batch_size}'])
+        acc_gpu = np.sum(np.array(actual_labels) == np.array(pred_labels))/len(actual_labels)
+
+        results = pd.DataFrame(columns = [f'GPU_{saved_model_name}_{batch_size}'])
         results.loc['batch_size']              = [batch_size]
-        results.loc['accuracy']                = [acc_tpu]
-        results.loc['first_prediction_time']   = [first_iter_time * 1000]
-        results.loc['average_prediction_time'] = [np.mean(iter_times) * 1000]
-        results.loc['load_time']               = [load_time]
-        results.loc['wall_time']               = [time.time() - walltime_start]
+        results.loc['accuracy']                = [acc_gpu]
+        results.loc['prediction_time']         = [np.sum(iter_times)*1000]
+        results.loc['images_per_sec_mean']     = [np.mean(batch_size / iter_times)]
+        results.loc['first_iteration_time']   = [first_iter_time * 1000]
+        results.loc['average_iteration_time'] = [np.mean(iter_times[1:]) * 1000]
+        results.loc['load_time']               = [load_time*1000]
+        results.loc['wall_time']               = [(time.time() - walltime_start)*1000]
 
         return results, iter_times
 
 
-results = pd.DataFrame()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model',default='resnet50' , type=str )
+    parser.add_argument('--batch_list',default=[1,8,16,32,64,128,256,512], type=list)
 
-for batch_size in batch_list:
-  opt = {'batch_size': batch_size}
-  iter_ds = pd.DataFrame()
-  
-  print(f'{batch_size} start')
-  res, iter_times = inference(model_type, int(batch_size))
-  col_name = lambda opt: f'{model_type}_{batch_size}'
-  
-  iter_ds = pd.concat([iter_ds, pd.DataFrame(iter_times, columns=[col_name(opt)])], axis=1)
-  results = pd.concat([results, res], axis=1)
-  print(results)
-print(results)
-results.to_csv(f'{model_type}.csv')
+    model = parser.parse_args().model
+    batch_list = parser.parse_args().batch_list
 
+    os.environ["CUDA_VISIBLE_DEVICES"]="0"
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+
+    if gpus:
+        try:
+            tf.config.experimental.set_memory_growth(gpus[0], True)
+        except RuntimeError as e:
+            print(e)
+    
+    results = pd.DataFrame()
+
+    for batch_size in batch_list:
+        opt = {'batch_size': batch_size}
+        iter_ds = pd.DataFrame()
+        
+        print(f'{model}-{batch_size} start')
+        res, iter_times = inference(model, int(batch_size))
+        col_name = lambda opt: f'{model}_{batch_size}'
+        
+        iter_ds = pd.concat([iter_ds, pd.DataFrame(iter_times, columns=[col_name(opt)])], axis=1)
+        results = pd.concat([results, res], axis=1)
+        print(results)
+        results.to_csv(f'{model}.csv')
