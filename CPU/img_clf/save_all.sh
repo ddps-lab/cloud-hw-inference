fac4a4a8d68ac2ccce63c34bde13d54e7079ab3f
Subin <39542978+subeans@users.noreply.github.com>
2022-07-05 14:11:59 +0900

    upload file for image classification on CPU

---
+++ CPU/img_clf/save_all.sh
@@ -0,0 +1,7 @@
+# save
+python3 save_model.py --model resnet50
+python3 save_model.py --model inception_v3
+python3 save_model.py --model xception
+python3 save_model.py --model vgg16
+python3 save_model.py --model mobilenet_v2
+
