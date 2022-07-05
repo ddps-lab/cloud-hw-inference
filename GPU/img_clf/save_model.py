51b94725bf34f03db56b1d7a8ebc36ab53407f2f
Subin <39542978+subeans@users.noreply.github.com>
2022-07-05 13:09:04 +0900

    remain only used model

--- GPU/img_clf/save_model.py
+++ GPU/img_clf/save_model.py
@@ -10,28 +10,13 @@ from tensorflow.keras.applications import (
     inception_v3,
     mobilenet_v2
 )
-from tensorflow.keras.models import load_model, save_model
-
 
 models_detail = {
-    'mobilenet': mobilenet.MobileNet(weights='imagenet'),
     'mobilenet_v2': mobilenet_v2.MobileNetV2(weights='imagenet'),
     'resnet50': resnet50.ResNet50(weights='imagenet'),
-    'resnet101': resnet.ResNet101(weights='imagenet'),
-    'resnet152': resnet.ResNet152(weights='imagenet'),
-    'resnet50_v2': resnet_v2.ResNet50V2(weights='imagenet'),
-    'resnet101_v2': resnet_v2.ResNet101V2(weights='imagenet'),
-    'resnet152_v2': resnet_v2.ResNet152V2(weights='imagenet'),
     'inception_v3': inception_v3.InceptionV3(weights='imagenet'),
-    'inception_resnet_v2': inception_resnet_v2.InceptionResNetV2(weights='imagenet'),
-    'densenet121': densenet.DenseNet121(weights='imagenet'),
-    'densenet169': densenet.DenseNet169(weights='imagenet'),
-    'densenet201': densenet.DenseNet201(weights='imagenet'),
-    'nasnetmobile': nasnet.NASNetLarge(weights='imagenet'),
-    'nasnetlarge': nasnet.NASNetMobile(weights='imagenet'),
     'xception': xception.Xception(weights='imagenet'),
     'vgg16': vgg16.VGG16(weights='imagenet'),
-    'vgg19': vgg19.VGG19(weights='imagenet'),
 }
 
 
