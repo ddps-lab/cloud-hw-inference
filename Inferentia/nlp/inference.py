2b6953a1a76794d10169417be0cf656a312ad81f
최재강 <chl8273@naver.com>
2022-07-05 15:13:49 +0900

    add user batch, compiled batch

--- Inferentia/nlp/inference.py
+++ Inferentia/nlp/inference.py
@@ -18,29 +18,30 @@ model_name = model_type = 'bert-base-uncased'
 #benchmark batch 128 neuron model
 neuron_b128_times = []
 batch_sizes = [1, 2, 4, 8, 16, 32, 64]
-for batch in batch_sizes:
-    compiled_model_dir = f'{model_type}_batch_{batch_size}_inf1'
+compiled_batch_sizes = [1, 2, 4, 8, 16, 32, 64]
+for compiled_batch in compiled_batch_sizes:
+    compiled_model_dir = f'{model_type}_batch_{compiled_batch_size}_inf1'
     inf1_compiled_model_dir = os.path.join(inf1_model_dir, compiled_model_dir)
     loaded_model = tf.keras.models.load_model(inf1_compiled_model_dir)
-    
-    seq_length = 128
-    dtype = "int32"
-    inputs = np.random.randint(0, 2000, size=(batch_size, seq_length)).astype(dtype)
-    for i in range(10):
-        start = time.time()
-        outputs = loaded_model(inputs)
-        end = time.time()
-        neuron_b128_times.append(end - start)
+    for batch_size in batch_sizes:
+        seq_length = 128
+        dtype = "int32"
+        inputs = np.random.randint(0, 2000, size=(batch_size, seq_length)).astype(dtype)
+        for i in range(10):
+            start = time.time()
+            outputs = loaded_model(inputs)
+            end = time.time()
+            neuron_b128_times.append(end - start)
 
-    neuron_b128_times = sorted(neuron_b128_times)
+        neuron_b128_times = sorted(neuron_b128_times)
 
-    print(f"Average throughput for batch 128 neuron model is {128/(sum(neuron_b128_times)/len(neuron_b128_times))} sentences/s.")
-    print(f"Peak throughput for batch 128 neuron model is {128/min(neuron_b128_times)} sentences/s.")
-    print()
+        print(f"Average throughput for batch 128 neuron model is {128/(sum(neuron_b128_times)/len(neuron_b128_times))} sentences/s.")
+        print(f"Peak throughput for batch 128 neuron model is {128/min(neuron_b128_times)} sentences/s.")
+        print()
 
 
-    print(f"50th percentile latency for batch 128 neuron model is {neuron_b128_times[int(1000*.5)] * 1000} ms.")
-    print(f"90th percentile latency for batch 128 neuron model is {neuron_b128_times[int(1000*.9)] * 1000} ms.")
-    print(f"95th percentile latency for bacth 128 neuron model is {neuron_b128_times[int(1000*.95)] * 1000} ms.")
-    print(f"99th percentile latency for batch 128 neuron model is {neuron_b128_times[int(1000*.99)] * 1000} ms.")
-    print()
+        print(f"50th percentile latency for batch 128 neuron model is {neuron_b128_times[int(1000*.5)] * 1000} ms.")
+        print(f"90th percentile latency for batch 128 neuron model is {neuron_b128_times[int(1000*.9)] * 1000} ms.")
+        print(f"95th percentile latency for bacth 128 neuron model is {neuron_b128_times[int(1000*.95)] * 1000} ms.")
+        print(f"99th percentile latency for batch 128 neuron model is {neuron_b128_times[int(1000*.99)] * 1000} ms.")
+        print()
