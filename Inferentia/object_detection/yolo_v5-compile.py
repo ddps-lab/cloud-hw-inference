f6ddfe03c15fc32f8d430ddbbed6189aaf70bda3
최재강 <chl8273@naver.com>
2022-07-05 12:55:21 +0900

    add diversity compiled batch

--- Inferentia/object_detection/yolo_v5-compile.py
+++ Inferentia/object_detection/yolo_v5-compile.py
@@ -1,70 +1,12 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Evaluate YOLO v3 on Inferentia
-# ## Note: this tutorial runs on tensorflow-neuron 1.x only
-
-# ## Introduction
-# This tutorial walks through compiling and evaluating YOLO v3 model on Inferentia using the AWS Neuron SDK.
-# 
-# 
-# In this tutorial we provide two main sections:
-# 
-# 1. Download Dataset and Generate Pretrained SavedModel
-# 
-# 2. Compile the YOLO v3 model.
-# 
-# 3. Deploy the same compiled model.
-# 
-# Before running the following verify this Jupyter notebook is running “conda_aws_neuron_tensorflow_p36” kernel. You can select the Kernel from the “Kernel -> Change Kernel” option on the top of this Jupyter notebook page.
-# 
-# Instructions of how to setup Neuron Tensorflow environment and run the tutorial as a Jupyter notebook are available in the Tutorial main page [Tensorflow-YOLO_v3 Tutorial](https://awsdocs-neuron.readthedocs-hosted.com/en/latest/neuron-guide/neuron-frameworks/tensorflow-neuron/tutorials/yolo_v3_demo/yolo_v3_demo.html)
-
-# ## Prerequisites
-# 
-
-# This demo requires the following pip packages:
-# 
-# `pillow matplotlib pycocotools`
-# 
-
-# In[3]:
-
-
 import sys
 get_ipython().system('{sys.executable} -m pip install pillow matplotlib pycocotools==2.0.2 --force --extra-index-url=https://pip.repos.neuron.amazonaws.com')
-    
-
-
-# ## Part 1:  Download Dataset and Generate Pretrained SavedModel
-# ### Download COCO 2017 validation dataset
-# 
-# We start by downloading the COCO validation dataset, which we will use to validate our model. The COCO 2017 dataset is widely used for object-detection, segmentation and image captioning.
-
-# In[4]:
-
 
 get_ipython().system('curl -LO http://images.cocodataset.org/zips/val2017.zip')
 get_ipython().system('curl -LO http://images.cocodataset.org/annotations/annotations_trainval2017.zip')
 get_ipython().system('unzip -q val2017.zip')
 get_ipython().system('unzip annotations_trainval2017.zip')
 
-
-# In[5]:
-
-
 get_ipython().system('ls')
-
-
-# 
-# ## Generate YOLO v3 tensorflow SavedModel (pretrained on COCO 2017 dataset)
-# 
-# Script yolo_v3_coco_saved_model.py will generate a tensorflow SavedModel using pretrained weights from https://github.com/YunYang1994/tensorflow-yolov3/releases/download/v1.0/yolov3_coco.tar.gz.
-
-# This tensorflow SavedModel can be loaded as a tensorflow predictor. When a JPEG format image is provided as input, the output result of the tensorflow predictor contains information for drawing bounding boxes and classification results.
-
-# In[7]:
-
 
 import json
 import tensorflow as tf
@@ -74,41 +16,6 @@ import matplotlib.patches as patches
 from tensorflow.keras.models import load_model
 
 yolo_model = './yolo_v5_coco_saved_model'
-
-# launch predictor and run inference on an arbitrary image in the validation dataset
-yolo_pred_cpu = load_model(yolo_model)
-image_path = './val2017/000000581781.jpg'
-with open(image_path, 'rb') as f:
-    feeds = {'image': [f.read()]}
-results = yolo_pred_cpu(feeds)
-
-# load annotations to decode classification result
-with open('./annotations/instances_val2017.json') as f:
-    annotate_json = json.load(f)
-label_info = {idx+1: cat['name'] for idx, cat in enumerate(annotate_json['categories'])}
-
-# draw picture and bounding boxes
-fig, ax = plt.subplots(figsize=(10, 10))
-ax.imshow(Image.open(image_path).convert('RGB'))
-wanted = results['scores'][0] > 0.1
-for xyxy, label_no_bg in zip(results['boxes'][0][wanted], results['classes'][0][wanted]):
-    xywh = xyxy[0], xyxy[1], xyxy[2] - xyxy[0], xyxy[3] - xyxy[1]
-    rect = patches.Rectangle((xywh[0], xywh[1]), xywh[2], xywh[3], linewidth=1, edgecolor='g', facecolor='none')
-    ax.add_patch(rect)
-    rx, ry = rect.get_xy()
-    rx = rx + rect.get_width() / 2.0
-    ax.annotate(label_info[label_no_bg + 1], (rx, ry), color='w', backgroundcolor='g', fontsize=10,
-                ha='center', va='center', bbox=dict(boxstyle='square,pad=0.01', fc='g', ec='none', alpha=0.5))
-plt.show()
-
-
-# ## Part 2:  Compile the Pretrained SavedModel for Neuron
-# 
-# We make use of the Python compilation API `tfn.saved_model.compile` that is available in `tensorflow-neuron<2`. For the purpose of reducing Neuron runtime overhead, it is necessary to make use of arguments `no_fuse_ops` and `minimum_segment_size`.
-# Compiled model is saved in ./yolo_v3_coco_saved_model_neuron.
-
-# In[27]:
-
 
 import tensorflow as tf
 import tensorflow.neuron as tfn
@@ -164,15 +71,12 @@ saved_model_dir = f'{model_type}_saved_model'
 
 
 # testing batch size
-batch_list = [1]
+batch_list = [1,2,4,8,16,32,64]
 num_of_cores = [1]
 for batch in batch_list:
     for core in num_of_cores:
         print('batch size:', batch,'core nums', core,'compile start')
         compile_inf1_model(saved_model_dir, inf1_model_dir, batch_size=batch, num_cores=core)
-
-
-# In[ ]:
 
 
 
