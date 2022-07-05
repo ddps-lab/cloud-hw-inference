d61a64464d7da0e42a38f46df780c6276c2001cf
Subin <39542978+subeans@users.noreply.github.com>
2022-07-05 13:16:42 +0900

    inference tensorflow model

---
+++ GPU/img_clf/inference_all.sh
@@ -0,0 +1,6 @@
+# inference for batch_list 
+python3 inference.py --model mobilenet_v2
+python3 inference.py --model resnet50
+python3 inference.py --model xception
+python3 inference.py --model inception_v3
+python3 inference.py --model vgg16
