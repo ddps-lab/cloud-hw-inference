58f478782086e54b99508e78a7dfe9185d2c178c
Subin <39542978+subeans@users.noreply.github.com>
2022-07-05 18:00:11 +0900

    inference Yolo with batch 1-64

---
+++ CPU/inference_all.sh
@@ -0,0 +1,7 @@
+python3 val.py --data data/coco.yaml --weights yolov5s_saved_model/ --batch-size 1
+python3 val.py --data data/coco.yaml --weights yolov5s_saved_model/ --batch-size 2
+python3 val.py --data data/coco.yaml --weights yolov5s_saved_model/ --batch-size 4
+python3 val.py --data data/coco.yaml --weights yolov5s_saved_model/ --batch-size 8
+python3 val.py --data data/coco.yaml --weights yolov5s_saved_model/ --batch-size 16
+python3 val.py --data data/coco.yaml --weights yolov5s_saved_model/ --batch-size 32
+python3 val.py --data data/coco.yaml --weights yolov5s_saved_model/ --batch-size 64
