ed5963540c5332a6f5676c9f858098b41cdb3fa2
최재강 <chl8273@naver.com>
2022-07-11 15:52:58 +0900

    rm num cores

--- Inferentia/object_detection/yolo_v5-inference.py
+++ Inferentia/object_detection/yolo_v5-inference.py
@@ -110,50 +110,6 @@ def analyze_bbox(results, batch_im_id, _clsid2catid):
 # Here is the actual evaluation loop. To fully utilize all four cores on one Inferentia, the optimal setup is to run multi-threaded inference using a `ThreadPoolExecutor`. The following cell is a multi-threaded adaptation of the evaluation routine at https://github.com/miemie2013/Keras-YOLOv4/blob/910c4c6f7265f5828fceed0f784496a0b46516bf/tools/cocotools.py#L97.
 from concurrent import futures
 
-def evaluate(yolo_predictor, images, eval_pre_path, anno_file, eval_batch_size, _clsid2catid, user_batch_size, num_cores):
-    batch_im_id_list, batch_im_name_list, batch_img_bytes_list = get_image_as_bytes(images, eval_pre_path, user_batch_size)
-    walltime_start = time.time()
-    iter_times = []
-    counter = 0
-    first_iter_time = 0
-    fut_im_list = []
-    fut_list = []
-    for batch_im_id, batch_im_name, batch_img_bytes in zip(batch_im_id_list, batch_im_name_list, batch_img_bytes_list):
-        if len(batch_img_bytes) != user_batch_size:
-            continue
-        iter_start = time.time()
-        fut = yolo_predictor({'image': np.array(batch_img_bytes, dtype=object)})
-        fut_im_list.append((batch_im_id, batch_im_name))
-        fut_list.append(fut)
-        if counter == 0:
-            first_iter_time = time.time() - iter_start
-        else:
-            iter_times.append(time.time() - iter_start)
-        counter +=1
-    bbox_list = []
-    counter = 0
-    for (batch_im_id, batch_im_name), fut in zip(fut_im_list, fut_list):
-        results = fut
-        bbox_list.extend(analyze_bbox(results, batch_im_id, _clsid2catid))
-        for _ in batch_im_id:
-            counter += 1
-            if counter % 100 == 0:
-                print('Test iter {}'.format(counter))
-    
-    print('==================== Performance Measurement ====================')
-    print('Finished inference on {} images in {} seconds'.format(len(images), time.time() - walltime_start))
-    print('=================================================================')
-    
-    results = pd.DataFrame(columns = [f'inf1_compiled_batch_size_{eval_batch_size}_compiled_cores_{num_cores}'])
-    results.loc['compiled_batch_size'] = [eval_batch_size]
-    results.loc['user_batch_size'] = [user_batch_size]
-    results.loc['first_prediction_time'] = [first_iter_time]
-    results.loc['average_prediction_time'] = [np.mean(iter_times)]
-    results.loc['wall_time'] = [time.time() - walltime_start]
-    box_ap_stats = bbox_eval(anno_file, bbox_list)
-    return box_ap_stats, results, iter_times
-
-
 import glob
 from PIL import Image
 
@@ -195,11 +151,11 @@ for user_batch in user_batchs:
     iter_ds = pd.DataFrame()
     results = pd.DataFrame()
     for eval_batch_size in batch_list:
-        opt ={'batch_size': eval_batch_size, 'num_cores': num_of_cores}
+        opt ={'batch_size': eval_batch_size}
         compiled_model_dir = f'{model_type}_batch_{eval_batch_size}'
         inf1_compiled_model_dir = os.path.join(inf1_model_dir, compiled_model_dir)
         print(f'inf1_compiled_model_dir: {inf1_compiled_model_dir}')
-        col_name = lambda opt: f'inf1_{eval_batch_size}_multicores_{num_cores}'
+        col_name = lambda opt: f'inf1_{eval_batch_size}'
 
         with open(val_annotate, 'r', encoding='utf-8') as f2:
             for line in f2:
@@ -231,18 +187,10 @@ for user_batch in user_batchs:
         results.loc['next_inference_time_median'] = [np.median(iter_times) * 1000]
         results.loc['load_time']               = [load_time * 1000]
         results.loc['wall_time']               = [(time.time() - walltime_start) * 1000]
-#             box_ap, res, iter_times = evaluate(yolo_pred,
-#                                                images,
-#                                                val_coco_root,
-#                                                val_annotate,
-#                                                eval_batch_size,
-#                                                clsid2catid,
-#                                                eval_batch_size * user_batch, 
-#                                                num_cores)
 
         iter_ds = pd.concat([iter_ds, pd.DataFrame(iter_times, columns=[col_name(opt)])], axis=1)
         results = pd.concat([results, res], axis=1)
     results.to_csv(f'{model_type}_batch_size_{batch_size}.csv')
-    display(results)
+    print(results)
 print(results)
 
